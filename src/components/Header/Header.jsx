8f8e737fbe87eb970b0767005579c3b4335d12b6
Olimpia Spyra <olimpia.spyra@gmail.com>
2023-01-31 19:54:46 +0100

    Add media queries

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,7 @@ import styles from './Header.module.scss';
 const Header = () => {
   return (
     <div className={styles.header}>
-      <h1 className={styles.titleTop}>Weekly to do list</h1>
-      <h1 className={styles.titleBottom}>Weekly to do list</h1>
+      <h1 className={styles.title}>Weekly to do list</h1>
       <div className={styles.background}></div>
     </div>
   );
